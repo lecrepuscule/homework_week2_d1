ea55193ee38398d10366fb90855e5d6c2e3cb277
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-17 22:20:05 +0100

    added basic html and css

--- soundboard/js/sound.js
+++ soundboard/js/sound.js
@@ -1,3 +1,57 @@
+var soundBoard = soundBoard || {};
+
+
+soundBoard.setup = function() {
+  this.playButton = document.getElementById("playbutton");
+  this.playButton.setAttribute("data-state", "stopped");
+  this.playButton.addEventListener("click", soundBoard.playClickHandler.bind(this))
+}
+
+
+soundBoard.getSound = function(){
+  if(!this.currentSound){
+    this.currentSound = soundManager.createSound({
+    id: "test",
+    url: "http://www.hazmatt.net/gaming/starcraft/terran/units/marine/tmardy00.wav"
+    })
+  }
+  return this.currentSound;
+}
+
+soundBoard.currentSound = null;
+
+
+
+soundBoard.playClickHandler = function(e) {
+  e.preventDefault();
+  console.log(this);
+  if(this.playButton.getAttribute("data-state") === "playing") {
+    this.pause();
+  } else {
+    this.play();
+  }
+}
+
+// {onfinish: this.clearSound.bind(this)}
+
+soundBoard.play = function(){
+  var sound = this.getSound();
+
+  if (this.playButton.getAttribute("data-state") ==="stopped") {
+    this.currentSound.play();
+  } else if (this.playButton.getAttribute("data-state") === "paused") {
+    this.currentSound.resume()
+  } else {
+    console.warn("soundBoard is in an unexpected state: " + this.playButton.getAttribute("data-state"));
+  }
+  this.playButton.setAttribute("data-state", "playing");
+}
+
+soundBoard.pause = function() {
+  this.currentSound.pause();
+  this.playButton.setAttribute("data-state", "paused");
+}
+
 
 
 soundManager.setup({
@@ -6,7 +60,7 @@ soundManager.setup({
   // optional: ignore Flash where possible, use 100% HTML5 mode
   // preferFlash: false,
   onready: function() {
-    console.log("ready");
+    soundBoard.setup().bind(soundBoard);
   }
 })
 
