4d160c4ed7a6d03d36efb1e3c6dc1e698974fe41
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-18 09:39:09 +0100

    removed some redudant comments

--- soundboard/js/sound.js
+++ soundboard/js/sound.js
@@ -4,55 +4,32 @@ soundBoard.currentSound = null;
 soundBoard.setup = function() {
   this.playButtons = document.getElementsByClassName("button");
   for (i=0; i< this.playButtons.length; i++) {
-    this.playButtons[i].setAttribute("data-state", "stopped");
+    // this.playButtons[i].setAttribute("data-state", "stopped");
     this.playButtons[i].addEventListener("click", soundBoard.playClickHandler)
   }
 }
 
-
-
 soundBoard.playClickHandler = function(e) {
   e.preventDefault();
-  // if(this.getAttribute("data-state") === "playing") {
-  //   soundBoard.pause();
-  // } else {
     soundBoard.play(this.id);
-  // }
 }
-
 
 soundBoard.play = function(character){
   soundManager.stopAll();
   soundManager.destroySound(character);
   var sound = this.getSound(character);
-  // if (this.getAttribute("data-state") ==="stopped") {
     this.currentSound.play();
-  // } else if (this.getAttribute("data-state") === "paused") {
-  //   soundBoard.currentSound.resume()
-  // } else {
-  //   console.warn("soundBoard is in an unexpected state: " + this.getAttribute("data-state"));
-  // }
-  // this.setAttribute("data-state", "playing");
 }
 
 soundBoard.getSound = function(character){
-  // if(!this.currentSound){
     var src = "sounds/"+character+"/"+Math.ceil(Math.random()*8)+".wav";
     console.log("src is " + src);
     soundBoard.currentSound = soundManager.createSound({
     id: character,
     url: src
     })
-  // }
   return soundBoard.currentSound;
 }
-
-// soundBoard.pause = function() {
-//   this.currentSound.pause();
-//   this.playButton.setAttribute("data-state", "paused");
-// }
-
-
 
 soundManager.setup({
   url: '../swf/',
